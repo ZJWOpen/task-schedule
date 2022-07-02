9b8f5aab995da5f2e3b88e318983b645b7b2757e
ZJWOpen <zhaosir_1993@163.com>
2022-07-02 08:59:45 +0800

    Add files via upload

--- example/main.go
+++ example/main.go
@@ -16,37 +16,43 @@ func main() {
 	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
 	for _, s := range a {
 
-		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		ctx, cancel := context.WithCancel(context.Background())
 		defer cancel()
 		task := schedule.NewTask(fmt.Sprintf("%d", s)).
 			WithRunFunc(runtestTaskfunc).
 			WithOnError(func(id string, err error) {
-				fmt.Println("task run failed ", id)
-				// t.Error(err)
+				fmt.Println("task run failed ", id, err)
 			}).
 			WithOnComplete(func(id string) {
-				fmt.Printf("task succes,%s", id)
-				fmt.Println()
+				fmt.Println("task succes,", id)
 			}).
 			WithContext(ctx).
-			WithCancel(cancel)
+			WithCancel(cancel).
+			WithTimeout(6 * time.Second)
 		err := sched.Add(task)
 		if err != nil {
 			fmt.Println(err)
 		}
-
 	}
 	time.Sleep(20 * time.Second)
 }
 
-func runtestTaskfunc(ctx context.Context, id string) error {
+func runtestTaskfunc(ctx context.Context, id string, timeout time.Duration) error {
+	done := make(chan struct{}, 1)
 	fmt.Println("task run ", id)
+	go func() {
+		time.Sleep(5 * time.Second)
+		done <- struct{}{}
+	}()
 	select {
 	case <-ctx.Done():
-		fmt.Println("task cancelled")
+		fmt.Println("task is cancelled", id)
 		return errors.New("task cancelled")
-	default:
-		time.Sleep(10 * time.Second)
+	case <-time.After(timeout):
+		fmt.Println("task timeout", id)
+		return errors.New("task timeout")
+	case <-done:
+		fmt.Println("run test task finished", id)
+		return nil
 	}
-	return nil
 }
